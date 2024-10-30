abb57aee4ef9f1da5dba5aa125fc2cc1cba1f522
piyush.a <piyush.a@infoedge.com>
2024-10-30 21:04:22 +0530

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   content: [
     "./packages/**/*.{js,jsx,ts,tsx}",
     "./packages/*/*.{js,jsx,ts,tsx}",
-        "./packages/**/*.{js,jsx,ts,tsx}", 
     "./packages/**/src/**/*.{js,jsx,ts,tsx}"
 
   ],
