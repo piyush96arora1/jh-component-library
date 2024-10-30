d83a915f6444fe39f247a42cbe78b0658be83cf9
piyush.a <piyush.a@infoedge.com>
2024-10-30 20:24:29 +0530

    changed padding

--- packages/jh-button/src/Button.tsx
+++ packages/jh-button/src/Button.tsx
@@ -14,7 +14,7 @@ export const Button = ({
   const baseStyles = "px-4 py-2 rounded";
   const variantStyles = {
     primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "bg-gray-500 text-white hover:bg-gray-600 px-3"
+    secondary: "bg-gray-500 text-white hover:bg-gray-600 px-4"
   };
 
   return (
