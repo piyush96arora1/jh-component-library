aae40b34a2ee04d1f9dad6b8d1bd84dc47ab3b9e
piyush.a <piyush.a@infoedge.com>
2024-10-30 20:41:39 +0530

    changed paddint to test

--- packages/jh-button/dist/index.esm.js
+++ packages/jh-button/dist/index.esm.js
@@ -1,1 +1,1 @@
-import e from"react";const r=({variant:r="primary",children:t,onClick:a})=>e.createElement("button",{className:`px-4 py-2 rounded ${{primary:"bg-blue-500 text-white hover:bg-blue-600",secondary:"bg-gray-500 text-white hover:bg-gray-600"}[r]}`,onClick:a},t);export{r as Button};
+import e from"react";const r=({variant:r="primary",children:t,onClick:a})=>e.createElement("button",{className:`px-4 py-2 rounded ${{primary:"bg-blue-500 text-white hover:bg-blue-600",secondary:"bg-gray-500 text-white hover:bg-gray-600 px-5"}[r]}`,onClick:a},t);export{r as Button};
--- packages/jh-checkbox/dist/index.js
+++ packages/jh-checkbox/dist/index.js
@@ -1,1 +1,1 @@
-"use strict";var e=require("react");exports.CheckBox=({variant:r="primary",children:t,onClick:a})=>e.createElement("button",{className:`px-4 py-2 rounded ${{primary:"bg-blue-500 text-white hover:bg-blue-600",secondary:"bg-gray-500 text-white hover:bg-gray-600"}[r]}`,onClick:a},t);
+"use strict";var e=require("react");exports.CheckBox=({variant:r="primary",children:t,onClick:a})=>e.createElement("button",{className:`px-4 py-2 rounded ${{primary:"bg-blue-500 text-white hover:bg-blue-600",secondary:"bg-gray-500 text-white hover:bg-gray-600 pd-2"}[r]}`,onClick:a},t);
