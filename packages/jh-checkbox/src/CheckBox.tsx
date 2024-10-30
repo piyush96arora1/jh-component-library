0c6262c0854523a28d6f26d0e700e2824167b7ed
piyush.a <piyush.a@infoedge.com>
2024-10-30 20:44:19 +0530

    changed paddint to test

--- packages/jh-checkbox/src/CheckBox.tsx
+++ packages/jh-checkbox/src/CheckBox.tsx
@@ -14,7 +14,7 @@ export const CheckBox = ({
   const baseStyles = "px-4 py-2 rounded";
   const variantStyles = {
     primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "bg-gray-500 text-white hover:bg-gray-600 pd-2"
+    secondary: "bg-gray-500 text-white hover:bg-gray-600 pd-3"
   };
 
   return (
